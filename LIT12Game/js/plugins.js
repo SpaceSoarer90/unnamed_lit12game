a080224f03d51ca01e31daaa64c8017c68710be2
redtrec <98370912+redtrec@users.noreply.github.com>
2024-11-02 23:01:00 +0800

    dsfdfsb

--- LIT12Game/js/plugins.js
+++ LIT12Game/js/plugins.js
@@ -9,5 +9,6 @@ var $plugins =
 {"name":"GALV_LayerGraphicsMZ","status":false,"description":"(v.1.3) Create graphic layers for parallax mapping, fog, etc. View documentation for plugin commands.","parameters":{"fixPosition":"true"}},
 {"name":"GALV_CharacterAnimationsMZ","status":false,"description":"(v.1.3) Make the player and follower characters use different\ngraphics for idle, walk and run. View HELP for more info.","parameters":{"useJumpGraphic":"true","vehicles":"false","commonEventId":"0","commonEventTime":"0","repeatCommonEvent":"true"}},
 {"name":"TDDP_PixelPerfect","status":true,"description":"1.1.1 Enable pixel perfect scaling mode for your game","parameters":{"enableIngameOptions":"false","labels":"","en":"Pixel Perfect Mode","ja":"ピクセルパーフェクトモード","zh":"像素完美模式","ko":"픽셀 퍼펙트 모드","ru":"Режим Pixel Perfect"}},
-{"name":"SHAZ_CharacterAnchor","status":false,"description":"Adjust anchor positions to change alignment with tiles.","parameters":{}}
+{"name":"SHAZ_CharacterAnchor","status":false,"description":"Adjust anchor positions to change alignment with tiles.","parameters":{}},
+{"name":"GALV_ActionIndicatorsMZ","status":true,"description":"(v.1.0) Display an icon when the player is able to interact with an event. View help for comment tag.","parameters":{"yOffset":"0","zPosition":"0","autoHide":"true","iconOpacity":"200"}}
 ];
